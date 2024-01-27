67d8fd26a9dcff4234fa2ef1308225e33187e998
docbogdanc <docbogdanc@yahoo.com>
2024-01-27 13:46:35 +0000

    changed fetched results to display in metric

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 var key = "7093b5895d7dff871294e9d20a842e17";
-var cityQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="
-// Boston&appid=7093b5895d7dff871294e9d20a842e17"
+
+
 
 $("#search-button").on("click", function () {
     event.preventDefault()
@@ -10,8 +10,8 @@ $("#search-button").on("click", function () {
 
     searchString = $("#search-input").val();
     console.log(searchString);
-    cityQueryURL = queryURL + searchString + "&appid=" + key;
-    // console.log('query: ', queryURL)
+    var cityQueryURL = queryURL + searchString + "&units=metric&appid=" + key;
+    
 
     fetch(cityQueryURL)
         .then(function (response) {

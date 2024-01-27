2db9baa0f5b6af3c69115b5ca47d3a77a94da371
docbogdanc <docbogdanc@yahoo.com>
2024-01-27 17:58:53 +0000

    populated first default weather data - London

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 var key = "7093b5895d7dff871294e9d20a842e17";
+var currentDay = dayjs().format("D/M/YYYY");
+console.log(currentDay);
 
-
+$('#currentCityDate').text("London ("+currentDay + ")")
+var cityQueryURL = queryURL + "London" + "&units=metric&appid=" + key;
+fetch(cityQueryURL)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+            $('#currentTemperature').text(" Temp: " + data.main.temp + "°C");
+            $('#currentWind').text(" Wind: " + data.wind.speed + "KPH");
+            $('#currentHumidity').text(" Humidity: " + data.main.humidity + "%");
+        });
+        
 
 $("#search-button").on("click", function () {
     event.preventDefault()
